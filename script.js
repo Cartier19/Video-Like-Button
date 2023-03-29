d303fe1df5a6e0d4d2f72b0b61baddcbe7d710fe
Olanrewaju Oluyomi <122275393+Cartier19@users.noreply.github.com>
2023-03-29 13:11:58 +0000

    Final commit

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 /* Write your solution here */
+const button = document.querySelector('button');
+button.addEventListener('click', () => {
+  var span = document.querySelector('.like-count').innerText = 1024
+  console.log('clicked')
+})
